0639955df8c26be05d72031b23765d1984b3a08b
Michael Dinerstein <md@missionmike.dev>
2025-02-21 05:56:52 +0000

    feat: initial commit.

---
+++ hello/hello.go
@@ -0,0 +1,30 @@
+package hello
+
+// User user type
+type User struct {
+	ID   int64
+	Name string
+	Addr *Address
+}
+
+// Address address type
+type Address struct {
+	City   string
+	ZIP    int
+	LatLng [2]float64
+}
+
+var michael = User{
+	ID:   1,
+	Name: "Michael",
+	Addr: &Address{
+		City:   "Newport",
+		ZIP:    97365,
+		LatLng: [2]float64{44.6368, 124.0535},
+	},
+}
+
+// Hello writes a welcome string
+func Hello() string {
+	return "Hello, " + michael.Name
+}
